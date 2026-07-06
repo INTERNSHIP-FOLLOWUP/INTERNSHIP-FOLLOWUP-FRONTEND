6f56b6143c59ff0e3fc78d2021da2608f3393206
Serey002 <serey.phem.12022006@gmail.com>
2026-07-06 11:47:44 +0700

    feat: #22 configure Tailwind CSS setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="bg-white rounded-2xl shadow-lg p-8 max-w-sm text-center">
+      <h1 class="text-3xl font-bold text-gray-800 mb-4">You did it!</h1>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>

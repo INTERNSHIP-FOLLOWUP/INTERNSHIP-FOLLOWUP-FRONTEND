14d1daef1cefde57d3af4ac5e67e879281bd68d5
Danemiok <kenk514912@gmail.com>
2026-07-09 09:31:35 +0700

    feat(views):#72 implement AdminUsersView panel wrapper interface

---
+++ src/views/user/AdminUsersView.vue
@@ -0,0 +1,18 @@
+<template>
+  <StudentList @view="handleView" @add="handleAdd" />
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import StudentList from '@/components/student/StudentList.vue'
+
+const router = useRouter()
+
+function handleView(id: number): void {
+  router.push(`/admin/users/${id}`)
+}
+
+function handleAdd(): void {
+  router.push('/admin/users/create')
+}
+</script>
